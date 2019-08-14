393d03ed454b00359bf0c32030cb165aa3b1a41f
shashankomre01 <shashank.omre07@gmail.com>
2019-08-14 10:34:15 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-
-sed -i 's/deployment/production/' server.js
+sed -i 's/devlopment/production/' server.js
 sed -i 's/staging/production/' server.js
-~                                               
+
